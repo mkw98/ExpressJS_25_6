f865d099d8b369007e7bfae179f772623581fb03
Maria Wieckowska <mkw98@gazeta.pl>
2019-02-23 19:25:54 +0100

    add login page

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 var express = require('express');
 var app = express();
-
 app.set('view engine', 'pug');
-app.set('views','./views');
-app.use('/store', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /store');
-    next();
+app.set('views', './views');
+
+app.get('/login', function (req, res) {
+    res.render('authentication');
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
-
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
-});
-
-app.get('/first-template', function(req, res){
-    res.render('first-template');
+app.get('/verified', function (req, res) {
+    res.render('content');
 });
 
 app.listen(3000);
 app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego czego żądasz');
 });
