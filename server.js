e85c4c97cb699c4701533b872f027b8e2fca29ff
Maria Wieckowska <mkw98@gazeta.pl>
2019-02-23 18:58:29 +0100

    edit server.js add views addtemplate.pug

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
 app.use('/store', function(req, res, next){
     console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
@@ -12,6 +14,10 @@ app.get('/', function (req, res) {
 
 app.get('/store', function (req, res) {
     res.send('To jest sklep');
+});
+
+app.get('/first-template', function(req, res){
+    res.render('first-template');
 });
 
 app.listen(3000);
